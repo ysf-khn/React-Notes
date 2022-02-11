8001c03d0868b714caf8b9daae46069a57d2b654
Yusuf <yusufmohd72@gmail.com>
2022-02-12 01:01:42 +0530

    Update and Delete actions left. CORS issue.

--- src/components/Notes/NoteItem.js
+++ src/components/Notes/NoteItem.js
@@ -4,21 +4,37 @@ import classes from "./NoteItem.module.css";
 const NoteItem = (props) => {
   const { isLoading, error, sendRequest: deleteNote } = useHttp();
 
+  const transformNotes = () => {};
+
   const deleteHandler = (noteId) => {
     console.log(noteId);
+    deleteNote(
+      {
+        url:
+          "https://react-http-b1160-default-rtdb.firebaseio.com/notes/" +
+          noteId,
+        method: "DELETE",
+      },
+      transformNotes
+    );
   };
 
   return (
     <div className={classes.note}>
-      <span
-        className={classes.del}
-        id={props.id}
-        onClick={() => deleteHandler(props.id)}
-      >
-        ❌
-      </span>
-      <h3>{props.title}</h3>
-      <p>{props.body}</p>
+      <div>
+        <h3>{props.title}</h3>
+        <p>{props.body}</p>
+      </div>
+      <div className={classes.buttons}>
+        <button className={classes.edit}>Edit</button>
+        <button
+          className={classes.del}
+          id={props.id}
+          onClick={() => deleteHandler(props.id)}
+        >
+          Delete
+        </button>
+      </div>
     </div>
   );
 };
